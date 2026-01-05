9ac64cfeaa24b0d7708b5ede587b19ede06c60b4
Juan_Carlos965 <160427321+Juansitor0@users.noreply.github.com>
2026-01-05 17:56:08 -0300

    Adicionando arquivos

---
+++ Jacaré/CLI/run.py
@@ -0,0 +1,41 @@
+import sys
+
+from Core.loader import load_csv_from_zip
+from Core.filters import filter_creditable, filter_board_name
+from Core.transformer import transform_inventory
+from Core.exporter import export_to_csv
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Uso correto:")
+        print("  python -m cli.run <arquivo_entrada.zip> <arquivo_saida.csv>")
+        sys.exit(1)
+
+    input_zip = sys.argv[1]
+    output_csv = sys.argv[2]
+
+    try:
+        print(" Carregando dados...")
+        df = load_csv_from_zip(input_zip)
+
+        print(" Aplicando filtros...")
+        df = filter_creditable(df)
+        df = filter_board_name(df)
+
+        print(" Transformando inventário...")
+        df_final = transform_inventory(df)
+
+        print("Exportando resultado...")
+        export_to_csv(df_final, output_csv)
+
+        print(" Processo concluído com sucesso!")
+
+    except Exception as e:
+        print(" Erro durante o processamento:")
+        print(str(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
